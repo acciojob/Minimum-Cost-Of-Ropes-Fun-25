3d3c0d14fc58025f4bbd0027681bb6131fa041be
Muskan <102637258+Fun-25@users.noreply.github.com>
2023-05-07 20:29:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+let arr=[4, 2, 7, 6, 9];
+
+function sortArr(arr){
+	arr.sort((a,b)=>a-b);
+	return arr;
+}
+fucntion minimumCost(){
+	let sum=0,elmt;
+
+	for(let i=0;i<arr.length;i++){
+		for(let j=0;j<2;j++){
+			sortArr(arr);
+			sum+=arr[0]+arr[1];
+			elmt=arr[0]+arr[1];
+
+			arr.splice(0,2,elmt);
+		}
+		
+	}
+}
+let cost=minimumCost(arr);
+console.log(cost)
