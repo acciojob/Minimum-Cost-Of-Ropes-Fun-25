8a1d4a33b5757cfb94805d49ddf4c8bd823e7fde
Muskan <102637258+Fun-25@users.noreply.github.com>
2023-05-07 20:36:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,63 @@
-//your code here
-let arr=[4, 2, 7, 6, 9];
+function minimumCost(arr) {
+  let totalCost = 0;
 
-function sortArr(arr){
-	arr.sort((a,b)=>a-b);
-	return arr;
+  // Create a min heap of ropes
+  let minHeap = [];
+  for (let i = 0; i < arr.length; i++) {
+    minHeap.push(arr[i]);
+  }
+  buildMinHeap(minHeap);
+
+  // Repeat until there is only one rope left
+  while (minHeap.length > 1) {
+    // Extract the two smallest ropes
+    let smallest1 = extractMin(minHeap);
+    let smallest2 = extractMin(minHeap);
+
+    // Connect the ropes and add the resulting rope to the queue
+    let newRope = smallest1 + smallest2;
+    totalCost += newRope;
+    minHeap.push(newRope);
+    heapifyUp(minHeap, minHeap.length - 1);
+  }
+
+  return totalCost;
 }
-fucntion minimumCost(){
-	let sum=0,elmt;
 
-	for(let i=0;i<arr.length;i++){
-		for(let j=0;j<2;j++){
-			sortArr(arr);
-			sum+=arr[0]+arr[1];
-			elmt=arr[0]+arr[1];
-
-			arr.splice(0,2,elmt);
-		}
-		
-	}
+function buildMinHeap(arr) {
+  for (let i = Math.floor(arr.length / 2); i >= 0; i--) {
+    heapifyDown(arr, i);
+  }
 }
-let cost=minimumCost(arr);
-console.log(cost)
+
+function extractMin(arr) {
+  let min = arr[0];
+  arr[0] = arr[arr.length - 1];
+  arr.pop();
+  heapifyDown(arr, 0);
+  return min;
+}
+
+function heapifyDown(arr, i) {
+  let left = 2 * i + 1;
+  let right = 2 * i + 2;
+  let smallest = i;
+  if (left < arr.length && arr[left] < arr[smallest]) {
+    smallest = left;
+  }
+  if (right < arr.length && arr[right] < arr[smallest]) {
+    smallest = right;
+  }
+  if (smallest !== i) {
+    [arr[i], arr[smallest]] = [arr[smallest], arr[i]];
+    heapifyDown(arr, smallest);
+  }
+}
+
+function heapifyUp(arr, i) {
+  let parent = Math.floor((i - 1) / 2);
+  if (parent >= 0 && arr[parent] > arr[i]) {
+    [arr[parent], arr[i]] = [arr[i], arr[parent]];
+    heapifyUp(arr, parent);
+  }
+}
